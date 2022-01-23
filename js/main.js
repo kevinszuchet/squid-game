9a259ec436b5c0881aeb55bea1e348543993d882
kevinszuchet <kevinszuchet@gmail.com>
2022-01-23 16:41:44 +0200

    load doll

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Renderer should be the size of our window.
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setClearColor(0xb7c3f3, 1)
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
 camera.position.z = 5;
 
-renderer.render( scene, camera );
+const loader = new THREE.GLTFLoader();
+loader.load("../models/scene.gltf", function(gltf) {
+  scene.add(gltf.scene);
+  gltf.scene.scale.set(.4, .4, .4);
+  gltf.scene.position.set(0, -1, 0);
+});
+
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+} 
+
+animate();
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
